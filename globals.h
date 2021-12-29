884daf5a4b6c86b25174492acaf05e3faabd7ce1
Jacobb626 <76225335+Jacobb626@users.noreply.github.com>
2021-12-29 04:03:35 -0500

    shit v2

--- globals.h
+++ globals.h
@@ -5,31 +5,5 @@
 namespace Globals
 {
 	inline UObject** GWorld;
-	inline UObject* GameplayStatics;
-	inline UObject* FortGameviewPortClient;
-	inline UObject* ConsoleClass;
-	inline UObject* CheatManagerClass;
 	inline UObject* PlayerController;
-
-	static inline void Init()
-	{
-		FortGameviewPortClient = FindObject(crypt("FortGameViewportClient /Engine/Transient.FortEngine_"));
-		GameplayStatics = FindObject(crypt("GameplayStatics /Script/Engine.Default__GameplayStatics"));
-		ConsoleClass = FindObject(crypt("/Script/Engine.Console"));
-		CheatManagerClass = FindObject(crypt("/Script/Engine.CheatManager"));
-	}
-
-	namespace Offsets
-	{
-		inline uintptr_t GameInstanceOffset;
-		inline uintptr_t LocalPlayersOffset;
-		inline uintptr_t PlayerControllerOffset;
-
-		static inline void Init()
-		{
-			GameInstanceOffset = FindOffset(crypt("World"), crypt("OwningGameInstance"));
-			LocalPlayersOffset = FindOffset(crypt("GameInstance"), crypt("LocalPlayers"));
-			PlayerControllerOffset = FindOffset(crypt("Player"), crypt("PlayerController"));
-		}
-	}
 }
